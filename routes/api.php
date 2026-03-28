156f145a3fb9cfc0a59347624e6327cc60d11a39
mohammed <mohammedabuamsha8@gamil.com>
2026-03-28 12:35:56 +0300

    edit folder

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('home',[testController::class, 'test']);
